5a2474a82657eee7132265c6c01184523770e398
Thomas Karchesy <tkarch01@seattlecentral.edu>
2016-04-07 17:46:16 -0700

    sandwich - wordpress- version

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,12 +6,14 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
         //associative array of data to pass to view
-        $data['title']="My Unique Title Tag!";
-        $data['page_id'] = "My Clever Page ID!";
+        $data['title']="My Unique Title Tag, now inside header!";
+        $data['page_id'] = "My Clever Page ID, in header!";
         
         
-        $data['guts'] ="<p>I am the creamy filling inside!</p>";
-        //data is passed tas second parameter in view creation
+        $data['guts'] ="<p>I am still the creamy filling inside!</p>";
+
+        $this->load->view('templates/header', $data);
 		$this->load->view('welcome_message', $data);
+        $this->load->view('templates/footer', $data);
 	}
 }
